c0f0bfbd294e4662a780100b7f466c019c76e779
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-06-15 11:35:31 -0700

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 let pokemonList = [
-    {name: 'Pikachu', type: ['electric', 'mouse'], height: 0.4},
-    {name: 'Magikarp', type: ['water', 'dragon'], height: 0.9},
-    {name: 'Scyther', type: ['bug', 'flying'], height: 1.5},
-]
+    {name: 'Pikachu', type: ['Electric', 'Mouse'], height: 0.4},
+    {name: 'Magikarp', type: ['Water', 'Dragon'], height: 0.9},
+    {name: 'Scyther', type: ['Bug', 'Flying'], height: 1.5},
+];
+
+for (let i=0; i<pokemonList.length; i++){
+    document.write("Name: "+ pokemonList[i].name +"<br>"+"Height: "+ pokemonList[i].height +"<br>"+"Type: "+ pokemonList[i].type +"<br>")
+   if (pokemonList[i].height<1){
+       document.write("I\'m just a wee little fella!"+"<br>")
+   }
+   else if (pokemonList[i].height>=1 && pokemonList[i].height<=3){
+       document.write("Big enough to do some damage!"+"<br>")
+   }
+   else if (pokemonList[i].height>3){
+       document.write("My sneezes sound like cannonfire!"+"<br>")
+   }
+
+   }
