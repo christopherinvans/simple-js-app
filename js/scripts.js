724a6626ced8edab25c21844342dfe162b2bad27
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-06-22 12:45:39 -0700

    changed for to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,6 @@ let pokemonList = [
     {name: 'Scyther', type: ['Bug', 'Flying'], height: 1.5},
 ];
 
-for (let i=0; i<pokemonList.length; i++){
-    document.write("Name: "+ pokemonList[i].name +"<br>"+"Height: "+ pokemonList[i].height +"<br>"+"Type: "+ pokemonList[i].type +"<br>")
-   if (pokemonList[i].height<1){
-       document.write("I\'m just a wee little fella!"+"<br>")
-   }
-   else if (pokemonList[i].height>=1 && pokemonList[i].height<=3){
-       document.write("Big enough to do some damage!"+"<br>")
-   }
-   else if (pokemonList[i].height>3){
-       document.write("My sneezes sound like cannonfire!"+"<br>")
-   }
-
-   }
+pokemonList.forEach(function(pokemon){
+    console.log(' Name: ' + pokemon.name + ' Type: ' + pokemonList.type + ' Height: ' + pokemon.height);
+} );
