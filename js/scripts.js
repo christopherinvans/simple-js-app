6ac1e4f830bd35cd32706f3db48cb89d27471bb9
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-06-22 13:21:31 -0700

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name: 'Pikachu', type: ['Electric', 'Mouse'], height: 0.4},
     {name: 'Magikarp', type: ['Water', 'Dragon'], height: 0.9},
     {name: 'Scyther', type: ['Bug', 'Flying'], height: 1.5},
-];
+    ];
 
-pokemonList.forEach(function (pokemon) {
-    document.write(`${pokemon.name} Height: ${pokemon.height} Type: ${pokemon.type} <br>`)
-})
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function(pokemon) {
+            return pokemonList;
+        }
+    };
+}) ();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu'});
+console.log(pokemonRepository.getAll());
+
+//pokemonList.forEach(function (pokemon) {
+   // document.write(`${pokemon.name} Height: ${pokemon.height} Type: ${pokemon.type} <br>`)
+//})
