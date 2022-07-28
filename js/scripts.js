445775336277cd1df66329ed7e197b8fbedc2ebb
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-07-28 14:14:06 -0700

    formatted for readability

--- js/scripts.js
+++ js/scripts.js
@@ -74,31 +74,23 @@ let pokemonRepository = (function () {
         modalContainer.innerHTML = "";
         let modal = document.createElement('div');
         modal.classList.add('modal');
+
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
         closeButtonElement.innerText = 'Close';
         closeButtonElement.addEventListener('click', hideModal);
+
         let titleElement = document.createElement('h1');
         titleElement.innerText = title;
+
         let contentElement = document.createElement('p');
         contentElement.innerText = text;
+
         modal.appendChild(closeButtonElement);
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
         modalContainer.appendChild(modal);
         modalContainer.classList.add('is-visible');
-        window.addEventListener('keydown', (e) => {
-            let modalContainer = document.querySelector('#modal-container');
-            if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-                hideModal();
-            }
-        });
-        modalContainer.addEventListener('click', (e) => {
-            let target = e.target;
-            if (target === modalContainer) {
-                hideModal();
-            }
-        });
     }
 
     document.querySelector('#show-modal').addEventListener('click', () => {
@@ -180,6 +172,21 @@ let pokemonRepository = (function () {
             showErrorMessage: showErrorMessage
         }
     })();
+
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
+
     return {
         add: add,
         getAll: getAll,
@@ -191,8 +198,6 @@ let pokemonRepository = (function () {
         hideModal: hideModal
     };
 }) ();
-
-const modalContainer = document.querySelector('.modal');
 
 pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon){
