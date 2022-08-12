d49e9013e108aff865936f951a373b523e7768d3
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-08-12 13:29:02 -0700

    removed email form
    and form validation function

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ let pokemonRepository = (function () {
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         listItem.classList.add('group-list-item')
+        listItem.classList.add('col-sm-4', 'col-md-6', 'col-lg-12');
         let buttonItem = document.createElement('button');
         buttonItem.innerText = pokemon.name;
         buttonItem.setAttribute("data-toggle", "modal");
@@ -124,84 +125,6 @@ let pokemonRepository = (function () {
 
 
     }
-
-
-
-    (function (){
-        let form = document.querySelector('#register-form');
-        let emailInput = document.querySelector('#email');
-        let passwordInput = document.querySelector('#password');
-
-        function showErrorMessage(input, message) {
-            let container = input.parentElement;
-            let error = container.querySelector('.error-message');
-            if (error) {
-                container.removeChild(error);
-            }
-            if (message) {
-                let error = document.createElement('div');
-                error.classList.add('.error-message');
-                error.innerText = message;
-                container.appendChild(error);
-            }
-        }
-
-        function validateEmail() {
-            let value = emailInput.value;
-            if (!value) {
-                showErrorMessage(emailInput, 'Email is a required field.');
-                return false;
-            }
-            if (value.indexOf('@') === -1) {
-                showErrorMessage(emailInput, 'You must enter a valid email address.');
-                return false;
-            }
-            if (value.indexOf('.') === -1) {
-                    showErrorMessage(emailInput, 'You must enter a valid e-mail address.');
-                    return false;
-            }    
-            
-            showErrorMessage(emailInput, null);
-            return true;
-        }
-
-        function validatePassword() {
-            let value = passwordInput.value;
-            if (!value) {
-                showErrorMessage(passwordInput, 'Password is a required field.');
-                return false;
-            }
-            if (value.length < 8) {
-                showErrorMessage(passwordInput, 'Password must be at least 8 characters.');
-                return false;
-            }
-            showErrorMessage(passwordInput, null);
-            return true;
-        }
-
-        function validateForm() {
-            let isValidEmail = validateEmail();
-            let isValidPassword = validatePassword();
-            return isValidEmail && isValidPassword;
-        }
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            if (validateForm()) {
-                alert('Success!');
-            }
-        });
-
-        emailInput.addEventListener('input', validateEmail);
-        passwordInput.addEventListener('input', validatePassword);
-
-        return {
-            validateEmail: validateEmail,
-            validatePassword: validatePassword,
-            validateForm: validateForm,
-            showErrorMessage: showErrorMessage
-        };
-    })();
 
     return {
         add: add,
